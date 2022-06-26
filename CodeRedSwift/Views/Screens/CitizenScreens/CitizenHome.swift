56e01aeeb141832414750964ba976d9d612b33a3
Soham Gupta <sohamgupta21@gmail.com>
2022-06-25 23:29:14 -0500

    fetch all

--- CodeRedSwift/Views/Screens/CitizenScreens/CitizenHome.swift
+++ CodeRedSwift/Views/Screens/CitizenScreens/CitizenHome.swift
@@ -6,10 +6,42 @@
 //
 
 import SwiftUI
+import YouTubePlayerKit
 
 struct CitizenHome: View {
     var body: some View {
-        Text("Hello, Citizen")
+        VStack {
+            Text("Hello, John Doe!")
+                .font(.system(size: 40))
+                .fontWeight(.bold)
+                .padding()
+            ScrollView {
+                YouTubePlayerView("https://www.youtube.com/watch?v=hizBdM1Ob68")
+                    .frame(width: 0.9 * UIScreen.main.bounds.width, height: 0.2 * UIScreen.main.bounds.height)
+                    .cornerRadius(15)
+                    .padding(.bottom)
+                YouTubePlayerView("https://www.youtube.com/watch?v=gDwt7dD3awc")
+                    .frame(width: 0.9 * UIScreen.main.bounds.width, height: 0.2 * UIScreen.main.bounds.height)
+                    .cornerRadius(15)
+                    .padding(.bottom)
+                YouTubePlayerView("https://www.youtube.com/watch?v=ebkQtGAqfis")
+                    .frame(width: 0.9 * UIScreen.main.bounds.width, height: 0.2 * UIScreen.main.bounds.height)
+                    .cornerRadius(15)
+                    .padding(.bottom)
+                YouTubePlayerView("https://www.youtube.com/watch?v=2v8vlXgGXwE")
+                    .frame(width: 0.9 * UIScreen.main.bounds.width, height: 0.2 * UIScreen.main.bounds.height)
+                    .cornerRadius(15)
+                    .padding(.bottom)
+                YouTubePlayerView("https://www.youtube.com/watch?v=FE7VSxA98a8")
+                    .frame(width: 0.9 * UIScreen.main.bounds.width, height: 0.2 * UIScreen.main.bounds.height)
+                    .cornerRadius(15)
+                    .padding(.bottom)
+                YouTubePlayerView("https://www.youtube.com/watch?v=CnbjPFwkiTQ")
+                    .frame(width: 0.9 * UIScreen.main.bounds.width, height: 0.2 * UIScreen.main.bounds.height)
+                    .cornerRadius(15)
+                    .padding(.bottom)
+            }
+        }
     }
 }
 
